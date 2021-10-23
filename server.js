072915c513a8efdc5f1bbcfe7b0a0bded68496a6
Ben Kaan <benkaan001@gmail.com>
2021-10-22 23:55:34 -0500

    add fs.writeFile

--- server.js
+++ server.js
@@ -41,6 +41,10 @@ app.post('/api/notes', (req,res) =>{
 function createNewNote(body, notesArray) {
     const note = body;
     notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
 
     //return finsihed code to post route for response
     return note;
